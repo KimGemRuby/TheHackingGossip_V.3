8252a452297783289f68ba697d98a12972191d9d
GXcode95 <xavier.grenouillet@gmail.com>
2021-08-02 16:53:17 +0200

    stylve v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root to: "gossip#index"
   get 'gossip/index'
   get 'gossip/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "gossip#index"
   get "/team", to: "student#index"
   get "/contacts", to: "student#contacts"
   get "/welcome/:first_name", to:  "student#welcome"
